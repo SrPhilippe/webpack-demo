b72bd39cb27b000426391963beb13ddc46163e2d
SrPhilippe <philippehenrique@outlook.com>
2021-08-15 08:57:51 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap';
+import './main.scss';
 import { FindUser } from "./app/sum.js";
 
 const user1 = new FindUser("Ricardo", 32);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
-const { NONAME } = require('dns')
-const path = require('path')
+const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    devtool: false,
     entry: './src/index.js',
-
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        publicPath: 'assets'
+    },
 }
