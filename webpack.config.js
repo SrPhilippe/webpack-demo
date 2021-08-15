8c75c82e4e598c4f67bfe59b521bbbd77027d3fd
SrPhilippe <philippehenrique@outlook.com>
2021-08-15 12:53:53 -0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     devtool: false,
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'main.[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test: /\s[ac]ss$/i,
+                test: /\.(sa|sc|c)ss$/i,
                 use: [
                     'style-loader',
                     'css-loader',
@@ -20,6 +21,9 @@ module.exports = {
             }
         ],
     },
+    plugins: [new HtmlWebPackPlugin({
+        template: './src/template.html'
+    })],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         publicPath: 'assets'
